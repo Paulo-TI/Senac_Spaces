e54c635997f0b3522b7d65a7c716625d97cfad04
aluno <prsf1986@gmail.com>
2023-03-18 10:58:58 -0300

    terceira atualização

--- galeria/views.py
+++ galeria/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
 
 def index (request):
-    return render(request, 'galeria/index.html')
+    return render(request, 'galeria/index.html', {'lista': [1, 2, 3, 4, 'tel', True]})
+
+def imagem (request):
+    return render(request, 'galeria/imagem.html')
+
